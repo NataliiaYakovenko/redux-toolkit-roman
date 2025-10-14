102a565ed03bb4fd6ed2a973ae4ca116b0433a85
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-10-14 11:16:27 +0300

    Add counterSlice

---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+  step: 1,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.count += state.step;
+    },
+    decrement: (state, action) => {
+      state.count -= state.step;
+    },
+  },
+});
+
+const { reducer, actions } = counterSlice;
+const { increment, decrement } = actions;
+
+export { increment, decrement };
+export default reducer;
