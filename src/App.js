6ae3a4ac58b3a206978c010ae319bfe61f71ea31
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-10-15 14:17:32 +0300

    Add component Header skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from "./components/Counter/Counter";
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Counter/>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <Header>
+      <h1>My counter</h1>
+      <nav>
+        <ul>
+          <li>link 1</li>
+          <li>link 2</li>
+          <li>link 3</li>
+        </ul>
+      </nav>
+
+      <div>
+        <button>Switch theme</button>
+      </div>
+    </Header>
+  );
+};
+
+export default Header;
