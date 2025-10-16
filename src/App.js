f5d4f00452da2be0e6d46e0d2e3ab9f4c42db1dc
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-10-16 13:33:44 +0300

    Add theme display for Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Counter from "./components/Counter/Counter";
 import Header from "./components/Header/Header";
 
-
 function App() {
   return (
     <div>
       <Header/>
-      <Counter/>
+      <Counter />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import cx from "classnames";
+import CONSTANTS from "../../constants";
 import styles from "./Header.module.scss";
+import { setTheme } from "../../store/slices/themeSlice";
 
-const Header = () => {
+const { THEMES } = CONSTANTS;
+
+const Header = ({ theme, language, setTheme }) => {
+  const className = cx(styles.header, {
+    [styles.darkTheme]: theme === THEMES.DARK,
+    [styles.lightTheme]: theme === THEMES.LIGHT,
+  });
+
   return (
-    <Header className={styles.header}>
+    <header className={className}>
       <h1>My counter</h1>
       <nav>
         <ul className={styles.nowList}>
           <li className={styles.nowListItem}>link 1</li>
-          <li>link 2</li>
-          <li>link 3</li>
+          <li className={styles.nowListItem}>link 2</li>
+          <li className={styles.nowListItem}>link 3</li>
         </ul>
       </nav>
 
       <div>
-        <button>Switch theme</button>
+        <button onClick={() => setTheme()}>Switch theme</button>
       </div>
-    </Header>
+    </header>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    theme: state.theme,
+    language: state.lang,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setTheme: () => dispatch(setTheme()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
